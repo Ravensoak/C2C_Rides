b6bcb99bd9619bc6947ddfdfdb50ba140055de05
Ravensoak <D.a.olney@outlook.com>
2026-05-07 07:16:08 +0100

    fdsds

--- js/meals.js
+++ js/meals.js
@@ -1,3 +1,9 @@
 /* =========================================================
   No sepecific scripting required for meals page, but this file is included in the HTML in case we want to add some later.
 ========================================================== */
+document.addEventListener("click", function (e) {
+  const link = e.target.closest("a[data-external]");
+  if (!link) return;
+  e.preventDefault();
+  window.open(link.href, "_system");
+});
